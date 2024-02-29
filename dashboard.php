9048b2ccf2cec1fa3f6e52739f2aa5f8eef2baaa
Andres Giraldo <agiraldodev@gmail.com>
2024-02-29 11:30:05 -0500

    Trayendo datos a la pantalla de Reporte

--- dashboard.php
+++ dashboard.php
@@ -33,9 +33,11 @@ include 'header.php';
   </div>
 
   <div class="boton">
-    <div class="boton-menu">
-      <i class="fa-solid fa-chart-line"></i>
-    </div>
+    <a href="reporte.php">
+      <div class="boton-menu">
+        <i class="fa-solid fa-chart-line"></i>
+      </div>
+    </a>
     <h3>Reporte Diario</h3>
   </div>
 </div>
